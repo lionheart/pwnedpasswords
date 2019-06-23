c0905762ac081513877c7be87a9ef7ea093f6113
Matt Hoffman <mjhoffman65@gmail.com>
2019-06-23 19:16:40 -0400

    updates number of matches in tests

--- test_pwnedpasswords.py
+++ test_pwnedpasswords.py
@@ -7,19 +7,19 @@ from . import pwnedpasswords
 class TestPwnedPasswords(unittest.TestCase):
     def setUp(self):
         self.value = "123123"
-        self.num_matches = 2048411
+        self.num_matches = 2194818
         self.password = pwnedpasswords.Password(self.value)
 
     def test_check(self):
-        assert(self.password.check() == self.num_matches)
+        self.assertEqual(self.password.check(), self.num_matches)
 
     def test_search(self):
-        assert(self.password.search() == self.num_matches)
+        self.assertEqual(self.password.search(), self.num_matches)
 
     def test_range(self):
         sha = "601F1889667EFAEBB33B8C12572835DA3F027F78"
         result = self.password.range().get(sha[5:])
-        assert(result == self.num_matches)
+        self.assertEqual(result, self.num_matches)
 
 if __name__ == "__main__":
     suite = unittest.TestLoader().loadTestsFromTestCase(TestPwnedPasswords)
